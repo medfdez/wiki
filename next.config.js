adfae5ece36a5d10e71d1aab8bb2f4c259780fee
Manuel Delgado <medfdez@gmail.com>
2022-10-12 18:17:36 -0400

    other

--- next.config.js
+++ next.config.js
@@ -84,4 +84,15 @@ module.exports = withBundleAnalyzer({
 
     return config
   },
-})
+});
+
+const debug = process.env.NODE_ENV !== "production";
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+    }
+  },
+  assetPrefix: !debug ? 'https://anotherplanet-io.github.io/Next-React-Components/' : '',
+}
